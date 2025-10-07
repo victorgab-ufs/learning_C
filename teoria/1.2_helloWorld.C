f017930b566027924357548f3f2f9ab830552e84
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-07 09:15:59 -0300

    atualizações_minicurso

--- teoria/1.2_helloWorld.C
+++ teoria/1.2_helloWorld.C
@@ -1,6 +1,6 @@
 // include -> importa a biblioteca (programa já pronto)
 
-#include <stdio.h>
+#include <stdio.h> // Cabeçalho (inclusão de bibliotecas)
 #include <stdlib.h>
 
 // stdio -> standard input output
@@ -8,20 +8,24 @@
 // input (entrada) -> scanf();
 // output (saída) -> printf();
 
-// função main
-
-int main(){
-
-    // corpo da função (entre chaves)
-
-    printf("Hello, World!\n");
-
-    // \n -> quebra de linha
-
-    system("pause");
-    
-    // Pede para o usuário apertar uma tecla para continuar
-    // Útil ao executar pelo windows, para ver o output antes da janela sumir
-
-    // ponto e vírgula necessário para compilar
-}
+int main(){ // assinatura da função main (função principal -> inicia o programa -> retorna um inteiro) //
+                                                                                                       //
+    // corpo da função (entre chaves)                                                                  //
+                                                                                                       //
+    printf("Hello, World!\n"); // corpo da função (instruções)                                         //
+                                                                                                       // 
+    // \n -> quebra de linha                                                                           //
+                                                                                                       // 
+    system("pause");                                                                                   //
+                                                                                                       //
+    // Pede para o usuário apertar uma tecla para continuar                                            // Escopo da função (espaço entre as chaves)
+    // Útil ao executar pelo windows, para ver o output antes da janela sumir                          //
+                                                                                                       //
+    // ponto e vírgula necessário para compilar                                                        //
+                                                                                                       //
+    return 0;                                                                                          //
+                                                                                                       //
+    // retorno da função main / indica o final do programa / 0 -> indica para o SO que o sistema       // 
+    // iniciou sem erros                                                                               //
+                                                                                                       //
+}                                                                                                      
