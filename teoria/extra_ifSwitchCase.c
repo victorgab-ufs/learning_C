844dc64e31b5b89d499f80369396c7af8cd7e0d3
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-08 22:53:32 -0300

    Create extra_ifSwitchCase.c

---
+++ teoria/extra_ifSwitchCase.c
@@ -0,0 +1,28 @@
+// if (condição1) {
+//   comando;
+// }
+//
+// else if (condição 2) {
+//    comando;
+//}
+//
+// else {
+//   comando;
+// }
+//
+// && (e) 
+// || (ou)
+// ! (não)
+// > (maior que) >= (maior ou igual)
+// < (menor que) <= (menor ou igual)
+// == (igual)
+// != (diferente)
+//
+// switch(variavel){
+//   case constante1: 
+//     comando;
+//     break;
+//     ...
+//   default: -> equivalente ao else
+//     comando;
+// }
