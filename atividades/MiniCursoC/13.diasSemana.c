9180a6c5e8767692c35171f8654aa4d4508a08c8
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-08 22:57:02 -0300

    Create 13.diasSemana.c

---
+++ atividades/MiniCursoC/13.diasSemana.c
@@ -0,0 +1,19 @@
+#include <stdio.h>>
+
+int main(){
+
+    int dia;
+    scanf("%d", &dia);
+
+    switch(dia){
+        case 1: printf("Domingo\n"); break;
+        case 2: printf("Segunda\n"); break;
+        case 3: printf("Terça\n"); break;
+        case 4: printf("Quarta\n"); break;
+        case 5: printf("Quinta\n"); break;
+        case 6: printf("Sexta\n"); break;
+        case 7: printf("Sábado\n"); break;
+        default: printf("Dia inválido\n"); break;
+    }
+    return 0;
+}
