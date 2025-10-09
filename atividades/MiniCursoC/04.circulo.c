6cb33b99f4d85fa760c6768dc13899989d3ec78f
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-08 21:50:54 -0300

    Create 04.circulo.c

---
+++ atividades/MiniCursoC/04.circulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+    float raio = 0;
+    scanf("%f", &raio);
+    
+    printf("Area = %.2f\nPerimetro = %.2f\n", 3.14*raio*raio, 2*3.14*raio);
+    
+    return 0;
+}
