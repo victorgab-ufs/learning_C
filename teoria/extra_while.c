8568c9a4d42073fd7bd8bcd1354a8314f118108e
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-09 09:13:08 -0300

    Create extra_while.c

---
+++ teoria/extra_while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  
+  // laço controlado por uma condição de parada (while -> enquanto)
+  int i = 1;  // inicialização
+  while (i <= 5) { 
+    printf("%d\n", i); // condição
+    i++; // incremento (essencial)
+  }
+  
+  return 0;
+}
