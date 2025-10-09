669f0124332b0cee2686b7fe0e6fb9e577d33df5
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-09 09:32:33 -0300

    Create 21.login.c

---
+++ atividades/MiniCursoC/21.login.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int senha = 1234;
+    int tentativa = 0;
+
+    do {
+        printf("Insira a senha:\n");
+        scanf("%d", &tentativa);
+        printf("Senha incorreta!\n\n");
+    } while (senha != tentativa);
+    
+    printf("Acesso permitido\n");
+    
+    return 0;
+}
