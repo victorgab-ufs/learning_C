993d2d4cbff439ecc745bc5037e0b460cccae416
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-09 12:46:42 -0300

    Create extra_vetores.c

---
+++ teoria/extra_vetores.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+  // armazena elementos de um mesmo tipo de dado
+  // tipo vetor[tamanho];
+
+  // indices de 0 até tamanho-1
+  int numeros[2];
+  
+  return 0;
+}
