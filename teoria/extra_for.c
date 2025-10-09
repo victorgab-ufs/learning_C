4de707003f496713740967872cef9937b1a66011
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-09 09:23:09 -0300

    Create extra_for.c

---
+++ teoria/extra_for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+  // for (inicialização; condição; incremento){}
+
+  // imprime os numeros de 1 ate 5
+  for (int i = 1; i <= 5; i++){
+    printf("%d\n", i);
+  }
+  return 0;
+}
