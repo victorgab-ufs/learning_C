e1444be51365d3a73c319831deb1cb5eaf1c33e7
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-10 15:55:19 -0300

    Create extra_strcmp.c

---
+++ teoria/extra_strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+// == 0 -> IGUAIS
+// < 0 -> string1 < string2 (lexicograficamente*)
+// > 0 -> string1 > string2 (lexicograficamente*)
+// * -> Ordem alfabética
+
+int main(){
+
+  char nome[100] = "Gustavo"
+  
+  
+
+  return 0;
+}
