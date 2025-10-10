8bbdae1af9880b7b6688eb80337179fc71183d83
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-10 13:58:57 -0300

    Create 31.verificarExistenciaArray.c

---
+++ atividades/MiniCursoC/31.verificarExistenciaArray.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+
+    int numeros[10] = {0};
+    int numero = 0;
+    int maior, menor = 0;
+    
+    printf("Insira os números da lista:\n");
+    
+    for (int i = 0; i < 10; i++){
+
+        scanf("%d", &numeros[i]);
+    }
+    
+    printf("\nInsira um numero para checar se esta na lista:\n");
+    
+    scanf("%d", &numero);
+
+    for (int j = 0; j < 11; j++){
+
+        if (j == 10){
+            printf("%d\n", -1);
+        }
+        
+        if (numero != numeros[j]){
+            continue;
+        }
+
+        else {
+            printf("O numero %d esta no indice %d\n", numero, j);
+            break;
+        }
+        
+    }
+
+    maior = numeros[1];
+    menor = numeros[1];
+
+    for (int k = 0; k < 9; k++){
+
+        if (numeros[k] > maior){
+            maior = numeros[k];
+        }
+
+        if (numeros[k] < menor){
+            menor = numeros[k];
+        }
+    }
+
+    printf("\nMaior elemento: %d\nMenor elemento: %d\n", maior, menor);
+
+    return 0;
+}}
