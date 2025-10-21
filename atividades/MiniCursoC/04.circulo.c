34f8bb6c399024e4e242b5976795b7bdeebee22a
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:35:31 -0300

    Update 04.circulo.c

--- atividades/MiniCursoC/04.circulo.c
+++ atividades/MiniCursoC/04.circulo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    
+
+    // inicialização da variável
     float raio = 0;
     scanf("%f", &raio);
-    
+
+    // resultado
     printf("Area = %.2f\nPerimetro = %.2f\n", 3.14*raio*raio, 2*3.14*raio);
     
     return 0;
