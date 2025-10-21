65329b22873995c3a149d342719a44905b146c41
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:33:39 -0300

    comentarios UltimoDigito

--- atividades/MiniCursoC/02.ÚltimoDígito.c
+++ atividades/MiniCursoC/02.ÚltimoDígito.c
@@ -2,13 +2,17 @@
 
 int main(){
 
+    // inicialização de variável
     int a = 0;
 
+    // input 
     printf("Escreva um número:\n");
     scanf("%d", &a);
 
+    // através da divisão por 10 é possível conseguir o último dígito do número (resto)
     int ultimo = a % 10;
 
+    // resultado
     printf("Ultimo dígito: %d\n", ultimo);
     
     return 0;
