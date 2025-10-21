57b357bb12a5741ee4772b09b85e8c82061fe429
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:03:11 -0300

    comentarios brinquedos

--- atividades/MiniCursoC/19.brinquedos.c
+++ atividades/MiniCursoC/19.brinquedos.c
@@ -2,26 +2,27 @@
 
 int main(){
 
+    // definicao de variaveis
     int altura, idade, qtd;
-
+    // input (altura do usuario)
     printf("Insira a altura em cm:\n");
     scanf("%d", &altura);
-
+    // input (idade do usuario)
     printf("Insira a idade em anos:\n");
     scanf("%d", &idade);
-
+    // 1° brinquedo exige idade 12+ anos e altura 150+ cm
     if (idade >= 12 && altura >= 150){
         qtd++;
     }
-
+    // 2° brinquedo exige idade 16+ anos e altura 170+ cm
     if (idade >= 16 && altura >= 170){
         qtd++;
     }
-
+    // 3° brinquedo exige idade 14+ anos e altura 140+ cm
     if (idade >= 14 && altura >= 140){
         qtd++;
     }
-
+    // imprime quantidade de brinquedos que o usuario pode utilizar de acordo com as condicoes estabelecidas
     printf("Ela pode andar em %d brinquedo(s)\n", qtd);
 
     return 0;
