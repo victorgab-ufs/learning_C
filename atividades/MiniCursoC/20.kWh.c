78757470137769b9903d43871ff8c8928fa30ce7
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:06:05 -0300

    Update 20.kWh.c

--- atividades/MiniCursoC/20.kWh.c
+++ atividades/MiniCursoC/20.kWh.c
@@ -2,31 +2,33 @@
 
 int main(){
 
+    // inicializacao das variaveis
     float valor = 0.0; 
     float consumo = 0.0;
-
+    // input (consumo em kWh)
     scanf("%f", &consumo);
 
-    if (consumo < 100) {
+    // consumo ate 100
+    if (consumo <= 100) {
 
         valor = consumo*0.25;
     }
-
-    else if (consumo >= 100 && consumo < 200){
+    // consumo maior que 100 ate 200
+    else if (consumo > 100 && consumo <= 200){
 
         valor = consumo*0.40;
     }
-
+    // consumo maior que 200 ate 300
     else if (consumo > 200 && consumo <= 300){
 
         valor = consumo*0.50;
     }
-
+    // consumo maior que 300
     else {
         
         valor = consumo*0.60;
     }
-
+    // output com valor a ser cobrado
     printf("O valor a ser cobrado é: R$ %.2f.\n", valor);
 
     return 0;
