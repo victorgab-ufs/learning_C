bca6987bd9b63fdc6a9e97564d65c2fcc3ad47ad
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:00:03 -0300

    comentarios meses

--- atividades/MiniCursoC/18.meses.c
+++ atividades/MiniCursoC/18.meses.c
@@ -2,11 +2,14 @@
 
 int main(){
 
+    // definicao de variavel
     int mes;
+    // input
     scanf("%d", &mes);
 
+    // switch-case comparando mes com os casos ligando o mes com o numero de 1 a 12 que os representa
     switch(mes){
-        
+        // numero dos meses e break para impedir a execucao dos casos seguintes
         case 1: printf("Janeiro\n"); break;
         case 2: printf("Fevereiro\n"); break;
         case 3: printf("Março\n"); break;
@@ -19,6 +22,7 @@ int main(){
         case 10: printf("Outubro\n"); break;
         case 11: printf("Novembro\n"); break;
         case 12: printf("Dezembro\n"); break;
+        // exececao: numeros fora do intervalo sao invalidos
         default: printf("Mês inválido\n"); break;
     }
 
