d3e8656d4caf11baffca798de270218ddf05d6b8
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 22:50:48 -0300

    comentarios paridadeSinal

--- atividades/MiniCursoC/14.paridadeSinal.c
+++ atividades/MiniCursoC/14.paridadeSinal.c
@@ -2,30 +2,34 @@
 
 int main(){
 
+    // inicializacao de variavel
     int n = 0;
+
+    // input
     scanf("%d", &n);
 
+    // checa se o numero eh par  
     if (n % 2 == 0){
-
+        // par + positivo
         if (n > 0) {
             printf("Par e positivo");
         }
-
+        // par + negativo
         else if (n < 0) {
             printf("Par e negativo");
         }
-
+        // par + nulo (nem positivo nem negativo)
         else {
             printf("Null");
         }
     }
-
+    // numero nao eh par (impar)
     else {
-
+        // impar + positivo
         if (n > 0){
             printf("Impar e positivo");
         }
-
+        // par + negativo
         else {
             printf("Impar e negativo");
         }
