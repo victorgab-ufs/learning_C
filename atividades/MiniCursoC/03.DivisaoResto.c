e3d0e8ffa9070b16e8b72a7c64fe7b4d8b388cbc
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:34:44 -0300

    comentarios DivisaoResto
    Added comments for variable initialization, user input, operations, and result.

--- atividades/MiniCursoC/03.DivisaoResto.c
+++ atividades/MiniCursoC/03.DivisaoResto.c
@@ -2,15 +2,19 @@
 
 int main(){
 
+    // inicialização das variáveis
     int a = 0;
     int b = 0;
 
+    // input do usuário
     printf("Escreva dois números:\n");
     scanf("%d %d", &a, &b);
 
+    // operações de divisão e resto
     int divisao = a / b;
     int resto = a % b;
-    
+
+    // resultado
     printf("Divisão: %d\nResto: %d\n", divisao, resto);
 
     return 0;
