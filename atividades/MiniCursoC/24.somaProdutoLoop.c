fbae306ec522c03d18b05dc723782b1358fa9898
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:27:07 -0300

    comentarios somaProdutoLoop

--- atividades/MiniCursoC/24.somaProdutoLoop.c
+++ atividades/MiniCursoC/24.somaProdutoLoop.c
@@ -2,10 +2,12 @@
 
 int main(){
 
+    // inicializacao das variaveis
     int soma = 0;
     int produto = 1;
+    // criacao de lista com ate 3 inteiros
     int numeros[3];
-    
+    // for que percorre a lista atribuindo o valor escaneado a posicao atual, o adiciona a soma e o multiplica ao produto
     for (int i = 0; i < 3; i++){
 
         scanf("%d", &numeros[i]);
@@ -13,7 +15,8 @@ int main(){
         produto *= numeros[i];
 
     }
-
+    //resultado
     printf("Soma: %d\nProduto: %d\n", soma, produto);
+    
     return 0;
 }
