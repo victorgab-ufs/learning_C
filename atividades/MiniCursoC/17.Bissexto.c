e65e70902331eb141d92d2b619f19e58271111cd
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 22:57:54 -0300

    comentarios bissexto

--- atividades/MiniCursoC/17.Bissexto.c
+++ atividades/MiniCursoC/17.Bissexto.c
@@ -2,14 +2,18 @@
 
 int main(){
 
+    // definicao de variavel
     int ano;
-    scanf("%d", &ano);
 
+    // input
+    scanf("%d", &ano);
+    
+    // checa se o ano eh multiplo de 4
     if (ano % 4 == 0){
 
         printf("Eh bissexto\n");
     }
-
+    
     else{
 
         printf("Nao eh bissexto\n");
