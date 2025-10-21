99aeb60e5b7b21200271a57b990398cd07beacbf
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 22:55:28 -0300

    comentarios triangulo

--- atividades/MiniCursoC/15.triangulo.c
+++ atividades/MiniCursoC/15.triangulo.c
@@ -2,19 +2,24 @@
 
 int main(){
 
+    // definicao de variaveis
     float a, b, c;
+    // input
     scanf("%f %f %f", &a, &b, &c);
 
+    // testa se os tres lados sao iguais
     if (a == b && b == c){
 
         printf("Equilatero\n");
     }
 
+    // testa se os tres lados sao diferentes
     else if (a != b && a != c && b != c){
 
         printf("Escaleno");
     }
 
+    // caso que sobrou: um dos lados é diferente
     else{
 
         printf("Isosceles");
