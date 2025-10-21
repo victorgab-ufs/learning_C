59971642fc71aebd5c60c1856e10f797d4357840
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 22:56:53 -0300

    comentarios menor.c

--- atividades/MiniCursoC/16.menor.c
+++ atividades/MiniCursoC/16.menor.c
@@ -2,19 +2,22 @@
 
 int main(){
 
+    // definicao de variaveis
     int a, b, c;
+    // input
     scanf("%d %d %d", &a, &b, &c);
 
+    // checa se a eh menor que os outros dois
     if (a < b && a < c) {
 
         printf("%d", a);
     }
-
+    // checa se o b eh menor que os outros dois
     else if (b < a && b < c) {
 
         printf("%d", b);
     }
-
+    // caso que sobrou: c ser menor que os outros dois
     else {
 
         printf("%d", c);
