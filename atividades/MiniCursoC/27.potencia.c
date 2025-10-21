ffc88344d46aaf49f12ad87b931f361836909bf6
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:37:05 -0300

    comentaios potencia

--- atividades/MiniCursoC/27.potencia.c
+++ atividades/MiniCursoC/27.potencia.c
@@ -2,27 +2,28 @@
 
 int main(){
 
+    // inicializacao das variaveis
     int base = 0;
     int expoente = 0;
     int potencia = 1;
 
     printf("Digite base e expoente separados por espaço:\n");
-
+    // recebendo valores de base e expoente
     scanf("%d %d", &base, &expoente);
-
+    // caso o expoente seja zero, o resultado da potencia eh 1
     if (expoente == 0){
 
         printf("%d\n", 1);
     }
-
+    // nao sendo...
     else{
-
+        // o valor da base eh multiplicado por ele mesmo n vezes, sendo n o valor do expoente
         for (int i = 1; i <= expoente; i++){
 
             potencia = potencia * base;
         }
     }
-
+    // imprime valor da potencia
     printf("%d\n", potencia);
 
     return 0;
