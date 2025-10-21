dd30e783071c2b006e31a2144c34edd9c5d3bf2e
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:30:03 -0300

    comentarios soma ate n

--- atividades/MiniCursoC/25.somaNwhile.c
+++ atividades/MiniCursoC/25.somaNwhile.c
@@ -2,18 +2,20 @@
 
 int main(){
 
+    // incializacao das variaveis
     int final = 1;
     int inicio = 1;
     int soma = 0;
-
+    // input (numero n da soma de 1 ate n)
     scanf("%d", &final);
-
+    // loop while que adiciona a soma o numero seguinte ate que se chegue a n
     while (inicio <= final){
 
         soma += inicio;
         inicio++;
     }
-
+    // resultado da soma de 1 ate n
     printf("Soma de %d até %d: %d\n", 1, final, soma);
+    
     return 0;
 }
