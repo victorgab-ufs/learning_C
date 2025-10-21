3cec7dc98e96c6c9ec92356808d0c52ca3b02580
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:37:56 -0300

    comentarios imc

--- atividades/MiniCursoC/06.IMC.c
+++ atividades/MiniCursoC/06.IMC.c
@@ -2,16 +2,21 @@
 
 int main(){
 
+    // definicao das variaveis
     float altura, peso, imc;
-    
+
+    // input para altura
     printf("Sua altura:\n");
     scanf("%f", &altura);
-    
+
+    // input para peso
     printf("Seu peso:\n");
     scanf("%f", &peso);
-    
+
+    // calculo do imc
     imc = peso/(altura*altura);
-    
+
+    // resultado formatado em duas casas decimais
     printf("Seu IMC = %.2f", imc);
     
     return 0;
