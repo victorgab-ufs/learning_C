c748910a8a65fe7a51c87f0e7d17197e91bae890
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:36:48 -0300

    comentarios Media3

--- atividades/MiniCursoC/05.Media3.c
+++ atividades/MiniCursoC/05.Media3.c
@@ -2,15 +2,19 @@
 
 int main(){
 
+    // inicialização das variáveis
     float nota1 = 0;
     float nota2 = 0;
     float nota3 = 0;
-    
+
+    // input do usuário
     scanf("%f %f %f", &nota1, &nota2, &nota3);
-
     
-
+    // cálculo da média
     float media = (nota1 + nota2 + nota3) / 3;
 
+    // resultado formatado para duas casas decimais
     printf("media = %.2f\n", media);
+
+    return 0;
 }
