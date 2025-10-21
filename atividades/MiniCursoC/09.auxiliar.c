a0cc2369478b4d9eb92169ead29246977a328db0
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:44:07 -0300

    comentarios auxiliar.c

--- atividades/MiniCursoC/09.auxiliar.c
+++ atividades/MiniCursoC/09.auxiliar.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
+// implementacao de funcao auxiliar (pedida no problema)
 int auxiliar(int n){
-    
+
+    // calculo pedido na funcao auxiliar
     int x = (n*n) + 3*n + 2;
-    
+
+    // retorno da funcao (int)
     return x;
 }
 
 int main(){
 
+    // inicializacao da variavel
     int numero = 0;
+
+    // input
     scanf("%d", &numero);
-    
+
+    // chamada da funcao auxiliar
     int resultado = auxiliar(numero);
+
+    // output
     printf("%d", resultado);
     
     return 0;
