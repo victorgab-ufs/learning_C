f7c707ce234817c7b71e6ba87410dc35c3725992
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 23:39:45 -0300

    comentarios somaDoWhile
    Added comments to clarify variable initialization and logic.

--- atividades/MiniCursoC/28.somaDoWhile.c
+++ atividades/MiniCursoC/28.somaDoWhile.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(){
-
+    // inicializacao das variaveis
     int n = 0;
     int acc = 0;
-
+    // escaneia um numero e o adiciona a soma (acc) enquanto esse numero for diferente de -1
     do {
         scanf("%d", &n);
         acc += n;
     } while (n != -1);
-
+    // imprime numero mais 1, evitando o problema do -1 interagir com o acc
     printf("%d", acc+1);
 
     return 0;
