7d70a84d91c9988cd2f2539aa4ee2e6114053158
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:31:46 -0300

    comentarios quadradoCubo

--- atividades/MiniCursoC/01.QuadradoCubo.c
+++ atividades/MiniCursoC/01.QuadradoCubo.c
@@ -2,14 +2,18 @@
 
 int main(){
 
+    // inicialização de variável
     int a = 0;
 
+    // pedindo entrada do usuário
     printf("Escreva um número:\n");
     scanf("%d", &a);
 
+    // operações através do inteiro digitado pelo usuário
     int aSquare = a*a;
     int aCube = a*a*a;
-    
+
+    // resultados
     printf("Quadrado: %d\nCubo: %d\n\n", aSquare, aCube);
 
     
