cc4229e3ed4b06a28b0e585bbc2b4a7a049285c6
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 22:47:33 -0300

    comentarios diasSemana

--- atividades/MiniCursoC/13.diasSemana.c
+++ atividades/MiniCursoC/13.diasSemana.c
@@ -2,10 +2,17 @@
 
 int main(){
 
+    // definicao da variavel
     int dia;
+    // input
     scanf("%d", &dia);
 
+    // estrutura switch-case
+    
+    // switch recebe uma variavel ja definida
     switch(dia){
+        // sao testados diversos casos, tendo quebra na execucao quando executado aquele de interesse, ataves do break
+        // case _situacao-desejada_: acao; break;  
         case 1: printf("Domingo\n"); break;
         case 2: printf("Segunda\n"); break;
         case 3: printf("Terça\n"); break;
@@ -13,7 +20,9 @@ int main(){
         case 5: printf("Quinta\n"); break;
         case 6: printf("Sexta\n"); break;
         case 7: printf("Sábado\n"); break;
+        // caso nenhum dos casos tenha sido validado, o default eh executado
         default: printf("Dia inválido\n"); break;
     }
+    
     return 0;
 }
