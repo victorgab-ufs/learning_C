0bf3e7867b81c7b08e3845900124481a0d4f7554
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-20 21:49:55 -0300

    comentarios segundos.c

--- atividades/MiniCursoC/11.segundos.c
+++ atividades/MiniCursoC/11.segundos.c
@@ -2,11 +2,16 @@
 
 int main(){
 
+    // definicao das variaveis
     int h, min, s;
-    
+
+    // input formatado
     scanf("%dh%dmin", &h, &min);
+
+    // calculo dos segundos totais (1 hora = 3600 segundos / 1 minuto = 60 segundos)
     s = (h*3600)+(min*60);
-    
+
+    // output
     printf("s = %d", s);
     
     return 0;
