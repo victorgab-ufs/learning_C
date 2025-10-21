386e27743322b75f1b4756a22a96d664415eb148
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-21 09:15:42 -0300

    comentarios mediaVetor

--- atividades/MiniCursoC/30.mediaVetor.c
+++ atividades/MiniCursoC/30.mediaVetor.c
@@ -2,22 +2,24 @@
 
 int main(){
 
+    // inicializacao das variaveis
     int array[5] = {0};
     int soma = 0.0;
-
+    
     printf("Insira 5 números:\n");
-
+    // leitura e soma simultanea dos valores da lista
     for (int i = 0; i < 5; i++){
         scanf("%d", &array[i]);
         soma += array[i];
     }
+    // impressao dos valores da lista
     printf("\nNúmeros digitados:\n");
     for (int j = 0; j < 5; j++){
         printf("%d\n", array[j]);
     }
-
+    // calculo da media dos valores do array
     float media = soma / 5.0; 
-
+    // impressao dos valores de soma e media
     printf("\nSoma: %d\nMedia: %.2f\n", soma, media);
 
     return 0;
